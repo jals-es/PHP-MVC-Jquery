9c2c1141cc05f2cc211c6f652828ff0b5a82b9d3
Juan Antonio López Seguí <31510870+jals-es@users.noreply.github.com>
2021-03-11 18:42:36 +0100

    Add files via upload

---
+++ model/connect.php
@@ -0,0 +1,33 @@
+<?php
+	function conn(){
+		$host = 'localhost';
+    	$user = "root";
+    	$pass = "";
+    	$db = "clase_appbar";
+    	$port = 3306;
+    	
+		$conn = new mysqli($host, $user, $pass, $db, $port);
+		
+		if($conn->connect_error){
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		return $conn;
+	}
+
+	function get_array($result){
+		if($result -> num_rows > 0){
+            $return = array();
+            $i = 0;
+            while($row = $result -> fetch_assoc()){
+                $return[$i] = $row;
+                $i++;
+            }
+        }else{
+            $return = false;
+		}
+		
+		return $return;
+	}
+
+?>
